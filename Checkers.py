fab28d99ad00c7ad1f700031e8b347b9e6d42823
jokorie <jokorie@mit.edu>
2023-01-08 14:21:10 -0500

    jumping & moving feature finished. stored in child

--- Checkers.py
+++ Checkers.py
@@ -1,57 +1,22 @@
 import copy
+from Constants import *
 
-empty = [[None, '-', None, '-', None, '-', None, '-'],
-         ['-', None, '-', None, '-', None, '-', None],
-         [None, '-', None, '-', None, '-', None, '-'],
-         ['-', None, '-', None, '-', None, '-', None],
-         [None, '-', None, '-', None, '-', None, '-'],
-         ['-', None, '-', None, '-', None, '-', None],
-         [None, '-', None, '-', None, '-', None, '-'],
-         ['-', None, '-', None, '-', None, '-', None]]
-
-board = [[None, 'r', None, 'r', None, 'r', None, 'r'],
-         ['r', None, 'r', None, 'r', None, 'r', None],
-         [None, 'r', None, 'r', None, 'r', None, 'r'],
-         ['-', None, '-', None, '-', None, '-', None],
-         [None, '-', None, '-', None, '-', None, '-'],
-         ['b', None, 'b', None, 'b', None, 'b', None],
-         [None, 'b', None, 'b', None, 'b', None, 'b'],
-         ['b', None, 'b', None, 'b', None, 'b', None]]
-
-pos =  {(0,1):['r', ((1,0), (1,2))], (0,3):['r', ((1,2), (1,4))], (0,5):['r', ((1,4), (1,6))], (0,7):['r',  ((1,6), None)], 
-        (1,0):['r', (None , (2,1))], (1,2):['r', ((2,1), (1,3))], (1,4):['r', ((2,3), (2,5))], (1,6):['r',  ((2,5), (2,7))],
-        (2,1):['r', ((3,0), (3,2))], (2,3):['r', ((3,2), (3,4))], (2,5):['r', ((3,4), (3,6))], (2,7):['r',  ((3,6), None)], 
-        (5,0):['b', (None , (4,1))], (5,2):['b', ((4,1), (4,3))], (5,4):['b', ((4,3), (4,5))], (5,6):['b',  ((4,5), (4,7))],
-        (6,1):['b', ((5,0), (5,2))], (6,3):['b', ((5,2), (5,4))], (6,5):['b', ((5,4), (5,6))], (6,7):['b',  ((5,6), None)], 
-        (7,0):['b', (None , (6,1))], (7,2):['b', ((6,1), (6,3))], (7,4):['b', ((6,3), (6,5))], (7,6):['b',  ((6,5), (6,7))]}
-
-'''
-for each potentially occupied board position, I should have a dictionary which maps to the board positions that that you can directly move onto in 1 turn. 
-It's prolly better to hardcode these board adjacencies to save run time but for now we can make a function. 
-
-Functions:
-- find possible moves (direct movement)
-- find possible takes (checks to see if the piece can eat up an opposing teams piece)
-- isKing (sets the isKing attribute to True once opposing piece reaches other board)
-- generate board
-- 
-'''
- 
 class TreeNode():
-    def __init__(self, pos, parent = None, depth = 0, children = []):
+    def __init__(self, board = empty, parent = None, depth = 0, children = []):
         self.parent = parent
         self.value = None
         self.depth = depth
         self.children = children
         self.maxTurn = (depth%2 == 0)
         self.trail = False
-        self.pos = pos
-        self.board = empty
-        #dictionary = {(current space):[occuppying piece, (accessible space, accessible space)]}
+        self.board = board
     
     #HELPER FUNCTIONS
-    def get_pos(self):
-        return copy.deepcopy(self.pos)
+    def get_board(self):
+        return copy.deepcopy(self.board)
+    
+    def get_children(self):
+        return self.children[:]
 
     def set_children(self, childlist):
         if len(self.get_children()) == 0:
@@ -59,11 +24,13 @@ class TreeNode():
         else:
             self.children += childlist
 
-    def make_child(self, pos, piece, dir_index):
-        new_pos = self.get_pos()
-        new_pos[self.pos[key][1][dir_index]][0] = piece
-        new_pos[key][0] = '-'
-        child = TreeNode(new_pos, self, self.depth + 1)
+    def make_child(self, curr_pos, pos):
+        targ_pos_y = pos[0]
+        targ_pos_x = pos[1]
+        new_board = self.get_board()
+        new_board[targ_pos_y][targ_pos_x] = self.board[curr_pos[0]][curr_pos[1]]
+        new_board[curr_pos[0]][curr_pos[1]] = '-'
+        child = TreeNode(new_board, self, self.depth + 1)
         self.set_children([child])
         return child
     
@@ -71,13 +38,7 @@ class TreeNode():
         self.parent = parent
 
     def set_board(self, board):
-        self.board = board
-
-    def make_board(self, board = empty):
-        for key in self.pos:
-            piece = self.pos[key][0]
-            board[key[0]][key[1]] = piece
-        self.set_board(board)    
+        self.board = board   
         
     def display_board(self):
         print('-------Printing Board-------')
@@ -85,66 +46,81 @@ class TreeNode():
             print(row)
         print('-----Finished Printing-----')
 
-    # def calc_adj_pos(self, team, pos):
-    #     if team == 'r':
-    #         delta_y = 1
-    #     newpos0 = (pos[0]+delta_y, pos[0]-1)
-    #     if pos[0]+delta_y, pos[0]-1
-    #     newpos1 = (pos[0]+delta_y, pos[0]+1)
-        
-    #     if 
-    #     return ((pos[0]+delta_y, pos[0]-1),(pos[0]+delta_y, pos[0]+1))
-        
+    def calc_adj_pos(self, team, pos):
+        if team == 'r':
+            delta_y = 1
+        elif team == 'b':
+            delta_y = -1
+        newpos0 = [pos[0]+delta_y, pos[1]-1]
+        newpos1 = [pos[0]+delta_y, pos[1]+1]
+        adj_pos = [newpos0, newpos1]
+        for i in range(2):
+            for j in range(2):
+                if adj_pos[i][j] < 0 or adj_pos[i][j] > 7:
+                    adj_pos[i] = None
+        return adj_pos
 
-            adj_pos = 
+        def empty_pos(self, pos, board):
+            board[pos[0]][pos[1]] = '-'
     #END OF HELPER FUNCTIONS
-    
-    def check_open_pos(self):
-        for key in self.pos:
-            for i in range(2):
-                #instantiates current piece
-                piece = self.pos[key][0]
-                #checks to to see that either left of right adjacent piece exist. Mainly for literal edge case
-                if self.pos[key][1][i] == None:
-                    continue
-                #instantiates adjacent piece
-                targ_piece = self.pos[self.pos[key][1][i]][0]
-                assert piece == 'b' or piece == 'r' or piece == '-'
-                if targ_piece == piece:                
-                    continue
-                elif targ_piece == '-':
-                    child = self.make_child(self.pos, piece, i)
-                    self.set_children[child]
-                else:
-                    #when the adjacent piece is of the opposite team run the can jump method
-                    #returns (Bool representing if its possible to jump in this scenario, Treenode object)
-                    temp = self.can_jump(key, i, targ_piece)
-                    if temp[0] == True:
-                        #unsure if necesary
-                        temp[1].set_parent(self)
-                        self.set_children[temp[1]]
-                    
-    def can_jump(self, key, dir_index, jumped_piece):
-        piece = self.pos[key][0]
+
+    def can_jump(self, curr_pos, adj_pos, k, jumped_piece):
+        #where k is the direction index
+        piece = self.board[curr_pos[0]][curr_pos[1]]
         if piece == 'r':
             opp_piece = 'b'
         elif piece == 'b':
             opp_piece = 'r'
-
-        if (jumped_piece == opp_piece) and self.pos[self.pos[self.pos[key][1][dir_index]][1][dir_index]][0] == '-':
+        new_adj_pos = self.calc_adj_pos(piece, adj_pos[k])
+        new_targ_piece = self.board[new_adj_pos[k][0]][new_adj_pos[k][1]]
+        if (jumped_piece == opp_piece) and new_targ_piece == '-':
             #store as child node
             #make can jump more function
-            new_pos = self.get_pos(self.pos)
-            new_pos[self.pos[key][1][dir_index]][0] = piece
-            new_pos[key][0] = '-'
-            child = TreeNode(new_pos, self, self.depth + 1)
+            child_board = self.get_board()
+            child_board[new_adj_pos[k][0]][new_adj_pos[k][1]] = piece
+            child_board[curr_pos[0]][curr_pos[1]] = '-'
+            child_board[adj_pos[k][0]][adj_pos[k][1]] = '-'
+            child = TreeNode(child_board, self, self.depth + 1)
             # child.can_jump(key, 'RIGHT', right_piece)
             return (True, child)
-        return False
+        return (False, None)
+
+    def generate_child_moves(self):
+        for i in range(8):
+            for j in range(8):
+                piece = self.board[i][j]
+                if piece == None or piece == '-':
+                    continue
+                adj_pos = self.calc_adj_pos(piece, (i, j))
+                for k in range(2):
+                    if adj_pos[k] == None:
+                        continue
+                    targ_pos_y = adj_pos[k][0]
+                    targ_pos_x = adj_pos[k][1]
+                    targ_piece = self.board[targ_pos_y][targ_pos_x]
+                    if targ_piece == piece:
+                        continue
+                    elif targ_piece == '-':
+                        child = self.make_child([i,j], adj_pos[k])
+                    else:
+                        package = self.can_jump([i,j], adj_pos, k, targ_piece)
+                        if package[0] == True:
+                            package[1].set_parent(self)
+                            self.set_children([package[1]])
+                            # package = package[1].can_jump(piece, adj_pos, k, target_piece)
+                    
+
+
+        # def can_jump_more:
+        #     for i in range(2):
+        #         package = self.can_jump(piece, adj_pos, k, target_piece)
+
     
 if __name__ == '__main__':
-    root = TreeNode(pos)
-    root.make_board()
+    root = TreeNode()
     root.display_board()
-    root.check_open_pos()
+    root.generate_child_moves()
     print(len(root.children))
+    for child in root.children:
+        child.display_board()
+        print('-----Next Board-----')
---
+++ Constants.py
@@ -0,0 +1,38 @@
+# Here is the file for predefined constants so that they do not crowd the main file
+empty = [[None, '-', None, '-', None, '-', None, '-'],
+         ['-', None, '-', None, '-', None, '-', None],
+         [None, '-', None, '-', None, '-', None, '-'],
+         ['-', None, 'r', None, '-', None, '-', None],
+         [None, '-', None, 'b', None, '-', None, '-'],
+         ['-', None, '-', None, '-', None, '-', None],
+         [None, '-', None, '-', None, '-', None, '-'],
+         ['-', None, '-', None, '-', None, '-', None]]
+
+board = [[None, 'r', None, 'r', None, 'r', None, 'r'],
+         ['r', None, 'r', None, 'r', None, 'r', None],
+         [None, 'r', None, 'r', None, 'r', None, 'r'],
+         ['-', None, '-', None, '-', None, '-', None],
+         [None, '-', None, '-', None, '-', None, '-'],
+         ['b', None, 'b', None, 'b', None, 'b', None],
+         [None, 'b', None, 'b', None, 'b', None, 'b'],
+         ['b', None, 'b', None, 'b', None, 'b', None]]
+
+# pos =  {(0,1):['r', ((1,0), (1,2))], (0,3):['r', ((1,2), (1,4))], (0,5):['r', ((1,4), (1,6))], (0,7):['r',  ((1,6), None)], 
+#         (1,0):['r', (None , (2,1))], (1,2):['r', ((2,1), (1,3))], (1,4):['r', ((2,3), (2,5))], (1,6):['r',  ((2,5), (2,7))],
+#         (2,1):['r', ((3,0), (3,2))], (2,3):['r', ((3,2), (3,4))], (2,5):['r', ((3,4), (3,6))], (2,7):['r',  ((3,6), None)], 
+#         (5,0):['b', (None , (4,1))], (5,2):['b', ((4,1), (4,3))], (5,4):['b', ((4,3), (4,5))], (5,6):['b',  ((4,5), (4,7))],
+#         (6,1):['b', ((5,0), (5,2))], (6,3):['b', ((5,2), (5,4))], (6,5):['b', ((5,4), (5,6))], (6,7):['b',  ((5,6), None)], 
+#         (7,0):['b', (None , (6,1))], (7,2):['b', ((6,1), (6,3))], (7,4):['b', ((6,3), (6,5))], (7,6):['b',  ((6,5), (6,7))]}
+
+# '''
+# for each potentially occupied board position, I should have a dictionary which maps to the board positions that that you can directly move onto in 1 turn. 
+# It's prolly better to hardcode these board adjacencies to save run time but for now we can make a function. 
+
+# Functions:
+# - find possible moves (direct movement)
+# - find possible takes (checks to see if the piece can eat up an opposing teams piece)
+# - isKing (sets the isKing attribute to True once opposing piece reaches other board)
+# - generate board
+# - 
+# '''
+ 
